6b113a20393b0cdff0794c7a391ec42b1cb8ef9f
inbouchet <83234731+inbouchet@users.noreply.github.com>
2023-03-26 16:43:01 +0200

    Update aruco_detection.py

--- pfe/src/aruco_detection.py
+++ pfe/src/aruco_detection.py
@@ -9,10 +9,21 @@ from pfe.msg import Dist
 import rospy
 
 class ArucoDetection:
+    """
+    This class implements Aruco marker detection using OpenCV library 
+    and publishes the distance and position of the marker from the 
+    camera in the form of custom message.
+    """
 
     bridge = CvBridge()
 
     def __init__(self):
+        
+        """ 
+        Initializes the node, subscribes to the camera topic, and publishes
+        the detected marker's distance and position.
+        """
+
         rospy.init_node("aruco_detection", anonymous=True)
 
         self.sub = rospy.Subscriber("/camera/image", Image, self.detection)
@@ -23,17 +34,29 @@ class ArucoDetection:
         rospy.spin()
 
     def detection(self, data):
+
+        """
+        Performs the Aruco marker detection and publishes the distance
+        and position of the detected marker.
+
+        Parameters:
+            - data (sensors_msg.msg.Image): The image data from the 
+              camera topic.
+        """
         try:
             img = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
 
+        # Load the camera matrix and distortion coefficients from file
         matrix = np.load("/home/inesbouchet/catkin_ws/src/pfe/src/MultiMatrix.npz")
         cam_matrix = matrix["camMatrix"]
         dist_coef = matrix["distCoef"]
 
+        # Define the dictionary used for Aruco marker detection
         dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_100)
 
+        # Define the parameters used for Aruco marker detection
         param = cv2.aruco.DetectorParameters()
 
         dist = Dist()
@@ -41,7 +64,9 @@ class ArucoDetection:
         corners, ids, rejected = cv2.aruco.detectMarkers(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), dict, parameters=param)
         
         if corners:
+            # Estimate the pose of the detected marker
             rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, 8, cam_matrix, dist_coef)
+            
             corners = np.asarray(corners)
 
             if corners.size == 8:
@@ -49,24 +74,21 @@ class ArucoDetection:
                 corners = corners.astype(int)
                 corners = corners.reshape(4,2)
 
+                # Calculate distance to marker and draw axes
                 distance = np.sqrt(tvec[0][0][2] ** 2 + tvec[0][0][0] ** 2 + tvec[0][0][1] ** 2)
 
                 point = cv2.drawFrameAxes(img, cam_matrix, dist_coef, rvec[0], tvec[0], 4, 4)
 
+                # Fill in data for message and publish
                 dist.origin_found = True
                 dist.dist_origin = distance
                 dist.x = round(tvec[0][0][0],1)
                 dist.y = round(tvec[0][0][1],1)
 
-                # x -> à gauche/à droite
-                # y -> en haut/en bas
-
                 if dist.origin_found:
                     print(" Detection : distance =", dist.dist_origin, "x =", dist.x, "y =", dist.y)
 
                 self.pub.publish(dist)
-
-            #cv2.imshow("Image", img)
 
         
         cv2.destroyAllWindows()
