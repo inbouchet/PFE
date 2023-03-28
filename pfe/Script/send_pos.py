b8bc20951a0fd5e3b7ad26320f893d5038acafb0
inbouchet <83234731+inbouchet@users.noreply.github.com>
2023-03-28 16:22:36 +0200

    Comment send_pos.py

--- pfe/Script/send_pos.py
+++ pfe/Script/send_pos.py
@@ -5,13 +5,28 @@ from std_msgs.msg import String
 from pfe.msg import Pos
 
 class Drone_Communication:
+    '''
+    The "Drone_Communication" class creates a ROS node named "talker" with a message editor to publish "Pos" messages on a topic named "chat". 
+    This class also creates a topic subscriber named "dist" to receive "Pos" messages.
+    '''
+
     def __init__(self):
+	
+	# Create a publisher to the "chatter" topic with message type Pos
         self.pub = rospy.Publisher('chatter', Pos, queue_size=10)
+	
+	# Create a subscriber to the "dist" topic with message type Pos
+    	# When a message is received, the callback() method is called to process it
         self.sub = rospy.Subscriber("dist",Pos,self.callback)
+	
+	# Initialize a ROS node named "talker"
         rospy.init_node('talker', anonymous=True)
 
     def callback(self,msg):
-
+	'''
+	The callback() method publish the received message on the "chat" topic and prints the information of the received message.
+	'''
+	
         self.pub.publish(msg)
         print(msg.x,msg.y,msg.dist_goal,msg.angle)
 
